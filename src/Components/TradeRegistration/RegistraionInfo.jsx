032a7a7bafad7fb03ac7775e9fd0d4c4f5ca7f67
Arslaan Ahmad <arslaanactive@gmail.com>
2024-09-20 15:41:09 +0530

    Biz registration done

--- src/Components/TradeRegistration/RegistraionInfo.jsx
+++ src/Components/TradeRegistration/RegistraionInfo.jsx
@@ -40,21 +40,21 @@ const RegistrationInfo = () => {
 
   return (
     <div className="max-w-4xl mx-auto py-12 px-6">
-      <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
-        <span className="text-red-500">Trade License</span> Registration
+      <h1 className="text-3xl font-bold text-center mb-8 text-gray-800 dark:text-gray-200">
+        <span className="text-green-500">Trade License</span> Registration
       </h1>
 
       {/* Overview Section */}
-      <div className="bg-gradient-to-r from-white via-gray-100 to-white p-8 rounded-lg shadow-lg mb-12">
-        <h2 className="text-2xl font-bold mb-6 text-gray-900 text-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg mb-12 dark:bg-neutral-800">
+        <h2 className="text-2xl font-bold mb-6 text-gray-900 text-center dark:text-gray-200">
           Overview
         </h2>
-        <div className="flex flex-col md:flex-row md:space-x-4">
+        <div className="flex flex-col md:flex-row md:space-x-4 ">
           {/* Left Column */}
           <div className="flex-1 md:pr-10 mb-6 md:mb-0">
             <ul className="space-y-4 text-gray-800">
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -71,15 +71,17 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
+                  <strong className="block font-semibold dark:text-gray-200">
                     Name of Approval / License:
                   </strong>
-                  <span className="text-gray-700">Trade License</span>
+                  <span className="text-gray-700 dark:text-gray-200">
+                    Trade License
+                  </span>
                 </div>
               </li>
 
-              <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+              <li className="flex items-start space-x-3 dark:text-gray-200">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -96,15 +98,17 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
+                  <strong className="block font-semibold dark:text-gray-100">
                     Issuing Authority:
                   </strong>
-                  <span className="text-gray-700">Urban Local Body</span>
+                  <span className="text-gray-700 dark:text-gray-300">
+                    Urban Local Body
+                  </span>
                 </div>
               </li>
 
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -121,13 +125,17 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">Stage:</strong>
-                  <span className="text-gray-700">Pre-Operation</span>
+                  <strong className="block font-semibold dark:text-gray-200">
+                    Stage:
+                  </strong>
+                  <span className="text-gray-700 dark:text-gray-200">
+                    Pre-Operation
+                  </span>
                 </div>
               </li>
 
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -144,23 +152,25 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
+                  <strong className="block font-semibold dark:text-gray-200">
                     SLA/ Number of Days:
                   </strong>
-                  <span className="text-gray-700">30 Days</span>
+                  <span className="text-gray-700 dark:text-gray-200">
+                    30 Days
+                  </span>
                 </div>
               </li>
             </ul>
           </div>
 
           {/* Vertical Divider */}
-          <div className="hidden md:block w-px bg-gray-300"></div>
+          <div className="hidden md:block w-px bg-gray-300 "></div>
 
           {/* Right Column */}
           <div className="flex-1 md:pl-4">
             <ul className="space-y-4 text-gray-800">
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -177,10 +187,10 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
-                    Documents Required:
+                  <strong className="block font-semibold dark:text-gray-200">
+                    Documents Requigreen:
                   </strong>
-                  <ul className="list-disc ml-6 text-gray-700 mt-1">
+                  <ul className="list-disc ml-6 text-gray-700 mt-1 dark:text-gray-200">
                     <li>ID Proof of Applicant</li>
                     <li>Address Proof of Applicant</li>
                   </ul>
@@ -188,7 +198,7 @@ const RegistrationInfo = () => {
               </li>
 
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -205,14 +215,14 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
+                  <strong className="block font-semibold dark:text-gray-200">
                     Form Submission:
                   </strong>
                   <a
                     href="http://niveshmitra.up.nic.in/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-red-600 hover:text-red-800 transition"
+                    className="text-green-600 hover:text-green-800 transition"
                   >
                     niveshmitra.up.nic.in
                   </a>
@@ -220,7 +230,7 @@ const RegistrationInfo = () => {
               </li>
 
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -237,15 +247,17 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">Fee:</strong>
-                  <span className="text-gray-700">
+                  <strong className="block font-semibold dark:text-gray-200">
+                    Fee:
+                  </strong>
+                  <span className="text-gray-700 dark:text-gray-200">
                     Variable Fee (Auto calculated based on inputs)
                   </span>
                 </div>
               </li>
 
               <li className="flex items-start space-x-3">
-                <span className="text-red-500">
+                <span className="text-green-500">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -262,10 +274,12 @@ const RegistrationInfo = () => {
                   </svg>
                 </span>
                 <div>
-                  <strong className="block font-semibold">
+                  <strong className="block font-semibold dark:text-gray-200">
                     Mode of Payment:
                   </strong>
-                  <span className="text-gray-700">Online</span>
+                  <span className="text-gray-700 dark:text-gray-200">
+                    Online
+                  </span>
                 </div>
               </li>
             </ul>
@@ -274,8 +288,8 @@ const RegistrationInfo = () => {
       </div>
 
       {/* Timeline Section */}
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4">
+      <div className="bg-white p-6 rounded-lg shadow-md dark:bg-neutral-800">
+        <h2 className="text-xl text-center font-bold mb-4">
           Procedure for Getting the License
         </h2>
 
@@ -284,7 +298,7 @@ const RegistrationInfo = () => {
             "Open the Online Single Window Portal and register using personal and contact details.",
             "After logging in, fill the common application form and create a unit.",
             'Select "Apply for Permission", choose unit, department ("Urban Development Department"), and service ("Trade License").',
-            "Fill the form, select license type, validity, upload required documents, and submit the application.",
+            "Fill the form, select license type, validity, upload requigreen documents, and submit the application.",
             'Go to "Pay Consolidated Fee" in the portal, select unit and service, and pay the calculated fee online.',
             "ULB’s verifying officer verifies the application and forwards it to the approving officer.",
             'The approving officer generates the order for the "Trade License".',
@@ -302,11 +316,11 @@ const RegistrationInfo = () => {
                   : "translate-x-full opacity-0"
               }`}
             >
-              <div className="absolute w-3 h-3 bg-red-500 rounded-full mt-1.5 -left-3.5 border border-white"></div>
-              <p className="text-lg font-semibold text-gray-700">
+              <div className="absolute w-3 h-3 bg-green-500 rounded-full mt-1.5 -left-3.5 border border-white"></div>
+              <p className="text-lg font-semibold text-gray-700  dark:text-gray-200">
                 Step {index + 1}
               </p>
-              <p className="text-gray-600 mt-2">{step}</p>
+              <p className="text-gray-600 mt-2 dark:text-gray-200">{step}</p>
             </div>
           ))}
         </div>
--- src/Components/TradeRegistration/RegistrationForm.jsx
+++ src/Components/TradeRegistration/RegistrationForm.jsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import handshake from "../../assets/Image/handshake.jpg";
 
 const RegistrationForm = () => {
+  useEffect(() => {
+    const form = document.getElementById("registration-form");
+    if (form) {
+      form.classList.add("opacity-0");
+      setTimeout(() => {
+        form.classList.remove("opacity-0");
+        form.classList.add("opacity-100");
+      }, 100);
+    }
+  }, []);
+
   return (
-    <div className="bg-white py-16">
+    <div className="bg-white dark:bg-neutral-800 py-16">
       <div className="max-w-4xl mx-auto px-4">
         {/* Title Section */}
-        <h1 className="text-4xl font-bold text-gray-900 text-center mb-8">
+        <h1 className="text-4xl font-bold text-gray-900 dark:text-white text-center mb-8">
           Trade <span className="text-red-500">Registration Form</span>
         </h1>
-        <p className="text-lg text-gray-600 text-center mb-12">
+        <p className="text-lg text-gray-600 dark:text-gray-300 text-center mb-12">
           Register your business with ease and start growing today. Sigma
           Solutions offers seamless trade registration services, guiding you
           through every step.
@@ -17,7 +29,7 @@ const RegistrationForm = () => {
         {/* Flex Container for Image and Form */}
         <div className="flex flex-col md:flex-row md:space-x-8">
           {/* Handshake Image */}
-          <div className="flex-1 mb-12 md:mb-0">
+          <div className="flex-1 mb-12 md:mb-0 transition-transform duration-300 hover:scale-105">
             <img
               src={handshake}
               alt="Business Handshake"
@@ -27,18 +39,21 @@ const RegistrationForm = () => {
 
           {/* Form Section */}
           <div className="flex-1">
-            <form className="space-y-4">
+            <form
+              id="registration-form"
+              className="space-y-4 transition-opacity duration-500 opacity-0"
+            >
               <div>
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >
                   Name
                 </label>
                 <input
                   type="text"
                   id="name"
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 dark:bg-neutral-700 dark:text-white sm:text-sm transition duration-200 hover:border-red-400"
                   placeholder="Enter your full name"
                 />
               </div>
@@ -46,14 +61,14 @@ const RegistrationForm = () => {
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 dark:bg-neutral-700 dark:text-white sm:text-sm transition duration-200 hover:border-red-400"
                   placeholder="Enter your email address"
                 />
               </div>
@@ -61,14 +76,14 @@ const RegistrationForm = () => {
               <div>
                 <label
                   htmlFor="company"
-                  className="block text-sm font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >
                   Company Name
                 </label>
                 <input
                   type="text"
                   id="company"
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500 dark:bg-neutral-700 dark:text-white sm:text-sm transition duration-200 hover:border-red-400"
                   placeholder="Enter your company name"
                 />
               </div>
@@ -76,7 +91,7 @@ const RegistrationForm = () => {
               <div>
                 <button
                   type="submit"
-                  className="w-full bg-red-500 text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition duration-200"
+                  className="w-full bg-red-500 text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition duration-200 transform hover:scale-105"
                 >
                   Register Now
                 </button>
