4741c7060f9227c8a3d03939b91f753f147cfd3f
Arslaan Ahmad <arslaanactive@gmail.com>
2024-09-27 11:48:16 +0530

    New

--- src/Components/AboutUs/Visson-Mission/CoreValue.jsx
+++ src/Components/AboutUs/Visson-Mission/CoreValue.jsx
@@ -62,6 +62,8 @@ const CoreValue = () => {
     },
   ];
 
+  //HELO
+
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg max-w-5xl">
       <h2 className="text-3xl font-bold text-green-500 mb-4">
